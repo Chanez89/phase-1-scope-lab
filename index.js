76b765811e805b006d53ad3b03971eedf695d8a2
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-08-31 15:31:25 -0700

    commit tree 7ae8c4aa4fc246b78f3dca4a56b29199fd3d7a16

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Assignment to constant variable.';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'failed attempt to change constant';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Fix the Scope
+    declare customerName to be bob in global scope
+      1) returns the customerName
+    upperCaseCustomerName()
+      2) modifies the customerName variable
+    setBestCustomer()
+      3) setBestCustomer
+    overwriteBestCustomer()
+      4) overwrites the best customer
+    changeLeastFavoriteCustomer()
+      5) unsuccessfully tries to reassign the least favorite customer
+      */
